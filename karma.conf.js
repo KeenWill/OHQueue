fecb390573d9c227cb5c8feb746027f244cd7edb
William Goeller <william@williamgoeller.com>
2019-10-25 02:58:51 -0400

    Turned off Karma auto-watch

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['Chrome'],
     customLaunchers: {
       Chrome_travis_ci: {
