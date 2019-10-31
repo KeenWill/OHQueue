a4c93061eee309d63e9765c6a63884b07e6da0bc
dlike230 <dlike@seas.upenn.edu>
2019-10-31 00:08:52 -0400

    styling fixes

--- src/app/pages/queue/queue/queue.component.ts
+++ src/app/pages/queue/queue/queue.component.ts
@@ -1,92 +1,92 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { QuestionService } from '../question.service';
-import { Observable, of } from 'rxjs';
-import { Queue } from '../models/queue.model';
-import { Question } from '../models/question.model';
+import {Component, OnInit, Input} from '@angular/core';
+import {QuestionService} from '../question.service';
+import {Observable, of} from 'rxjs';
+import {Queue} from '../models/queue.model';
+import {Question} from '../models/question.model';
 
-import { AskQuestionDialogComponent } from './ask-question-dialog.component';
-import { MatDialog } from '@angular/material/dialog';
-import { AuthService } from '../../../@core/auth/auth.service';
+import {AskQuestionDialogComponent} from './ask-question-dialog.component';
+import {MatDialog} from '@angular/material/dialog';
+import {AuthService} from '../../../@core/auth/auth.service';
 
-import { User } from '../../../@core/auth/auth.service';
-import { QueuesService } from '../queues.service';
-import { map } from 'rxjs/operators';
+import {User} from '../../../@core/auth/auth.service';
+import {QueuesService} from '../queues.service';
+import {map} from 'rxjs/operators';
 
 @Component({
-    selector: 'queue',
-    templateUrl: './queue.component.html',
-    styleUrls: ['./queue.component.scss'],
+  selector: 'queue',
+  templateUrl: './queue.component.html',
+  styleUrls: ['./queue.component.scss'],
 })
 export class QueueComponent implements OnInit {
 
-    @Input() queue: Queue;
+  @Input() queue: Queue;
 
-    showAnswered: boolean = true;
-    private unansweredQuestions: Observable<Question[]>;
-    private allQuestions: Observable<Question[]>;
+  showAnswered: boolean = true;
+  private unansweredQuestions: Observable<Question[]>;
+  private allQuestions: Observable<Question[]>;
 
-    constructor(
-        private questionService: QuestionService,
-        private queuesService: QueuesService,
-        public auth: AuthService,
-        public dialog: MatDialog) {
-    }
-  
-    timestampComparison(qa: Question, qb: Question): number {
-      if (qa.timeCreated < qb.timeCreated) return 1;
-      if (qa.timeCreated > qb.timeCreated) return -1;
-      return 0;
-    }
-  
-    servedComparison(qa: Question, qb: Question) {
-      if (qa.served && qb.served) return 0;
-      if (qa.served && !qb.served) return 1;
-      return -1;
-    }
+  constructor(
+    private questionService: QuestionService,
+    private queuesService: QueuesService,
+    public auth: AuthService,
+    public dialog: MatDialog) {
+  }
 
-    ngOnInit() {
-        this.allQuestions = this.questionService.getQuestions(this.queue.id).pipe(map(questions => questions
-            .sort((qa, qb) => timestampComparison(qa, qb) + 10 * servedComparison(qa, qb))));
-        this.unansweredQuestions = this.allQuestions.pipe(map(questions =>
-            questions.filter(question => !question.served)));
-    }
+  static timestampComparison(qa: Question, qb: Question): number {
+    if (qa.timestamp < qb.timestamp) return 1;
+    if (qa.timestamp > qb.timestamp) return -1;
+    return 0;
+  }
 
-    askQuestion(asker: User) {
-        const dialogRef = this.dialog.open(AskQuestionDialogComponent, {
-            width: '250px',
-            data: {},
-        });
+ static servedComparison(qa: Question, qb: Question) {
+    if (qa.served && qb.served) return 0;
+    if (qa.served && !qb.served) return 1;
+    return -1;
+  }
 
-        dialogRef.afterClosed().subscribe(result => {
-            this.questionService.createQuestion(asker, this.queue.id, result.title || '', result.desc || '');
-        });
-    }
+  ngOnInit() {
+    this.allQuestions = this.questionService.getQuestions(this.queue.id).pipe(map(questions => questions
+      .sort((qa, qb) => QueueComponent.timestampComparison(qa, qb) + 10 * QueueComponent.servedComparison(qa, qb))));
+    this.unansweredQuestions = this.allQuestions.pipe(map(questions =>
+      questions.filter(question => !question.served)));
+  }
 
-    showAnsweredQuestions() {
-        this.showAnswered = true;
-        // this.answeredQuestions = this.answeredQuestionsObs;
-    }
+  askQuestion(asker: User) {
+    const dialogRef = this.dialog.open(AskQuestionDialogComponent, {
+      width: '250px',
+      data: {},
+    });
 
-    hideAnsweredQuestions() {
-        this.showAnswered = false;
-        // this.answeredQuestions = of([]);
-    }
+    dialogRef.afterClosed().subscribe(result => {
+      this.questionService.createQuestion(asker, this.queue.id, result.title || '', result.desc || '');
+    });
+  }
 
-    answerQuestion() {
-        // this.questionService.answerQuestion(this.queue.id);
-        console.log('answering question');
-    }
+  showAnsweredQuestions() {
+    this.showAnswered = true;
+    // this.answeredQuestions = this.answeredQuestionsObs;
+  }
 
-    openQueue() {
-        this.queuesService.openQueue(this.queue.id);
-    }
+  hideAnsweredQuestions() {
+    this.showAnswered = false;
+    // this.answeredQuestions = of([]);
+  }
 
-    closeQueue() {
-        this.queuesService.closeQueue(this.queue.id);
-    }
+  answerQuestion() {
+    // this.questionService.answerQuestion(this.queue.id);
+    console.log('answering question');
+  }
 
-    deleteQueue() {
-        this.queuesService.deleteQueue(this.queue.id);
-    }
+  openQueue() {
+    this.queuesService.openQueue(this.queue.id);
+  }
+
+  closeQueue() {
+    this.queuesService.closeQueue(this.queue.id);
+  }
+
+  deleteQueue() {
+    this.queuesService.deleteQueue(this.queue.id);
+  }
 
 }
