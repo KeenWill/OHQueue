e2fc80fcbf34a077b6aa6ddfb2be29efee20bbdb
William Goeller <KeenWill@users.noreply.github.com>
2019-11-05 11:01:52 -0500

    Update src/app/@core/auth/auth.service.ts

--- src/app/@core/auth/auth.service.ts
+++ src/app/@core/auth/auth.service.ts
@@ -150,7 +150,7 @@ export class AuthService {
   }
 
   // Sets user data to firestore after succesful login
-  private updateUserData({ uid: string, email: string, displayName: string }): AngularFirestoreDocument<any> {
+  private updateUserData({ uid, email, displayName }): AngularFirestoreDocument<any> {
     return this.afs.doc(
       `users/${uid}`,
     )
